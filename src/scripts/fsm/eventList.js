d433c68db27e30e7802cc58615f36a2ff7dd16a5
Dmitry Khotko <dmitry.khotko@gmail.com>
2014-09-14 18:28:18 +0300

    forbid events with equal names in the same event list

--- src/scripts/fsm/eventList.js
+++ src/scripts/fsm/eventList.js
@@ -13,20 +13,23 @@
      */
     global.FSM.EventList = function () {
         var event,
+            names = {},
             i,
             len;
 
         // validate params and init
         // --------------------------------------------------------------------
 
-        if (arguments.length === 0) { throw "At least one FSM.Event should be provided"; }
+        if (arguments.length === 0) { throw "At least one event should be provided"; }
 
         for (i = 0, len = arguments.length; i < len; i++) {
             event = arguments[i];
 
-            if (!(event instanceof global.FSM.Event)) { throw "All arguments should be instances of FSM.Event"; }
+            if (!(event instanceof global.FSM.Event)) { throw "All events should be instances of FSM.Event"; }
+            if (names[event.name]) { throw "All events in event list should have unique names"; }
 
-            this[arguments[i].name] = arguments[i];
+            this[event.name] = event;
+            names[event.name] = true;
         }
 
         Object.defineProperty(this, 'length', { value: len });
